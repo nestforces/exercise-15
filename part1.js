ed7e27f88ee4373f530f481b7cf0c28a7512a121
nestforces <maulanamahfud2113@gmail.com>
2023-09-19 11:19:24 +0700

    Update part1.js (find GCD)

--- part1.js
+++ part1.js
@@ -5,3 +5,14 @@ function isWeekend(s) {
 }
 
 console.log(isWeekend("2023-09-17"));
+
+function findGCD(num1, num2) {
+    while (num2 !== 0) {
+      const temp = num2;
+      num2 = num1 % num2;
+      num1 = temp;
+    }
+    return num1;
+  }
+  
+  console.log(findGCD(36, 18));
