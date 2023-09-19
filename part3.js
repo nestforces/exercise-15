8dd5fe944d4853080e65d47d13650b4e4fd50fb9
Sando-Mutaz <143694001+Sando-Mutaz@users.noreply.github.com>
2023-09-19 11:33:19 +0700

    add getDaysInMonth

--- part3.js
+++ part3.js
@@ -16,3 +16,27 @@ function salinParuhPertama(array) {
 const array = [1, 2, 3, 4, 5];
 const hasil = salinParuhPertama(array);
 console.log(hasil);
+
+
+// No. 10
+/* Tulis kode untuk mendapatkan jumlah hari dalam sebulan */
+
+function getDaysInMonth(month, year) {
+  // Membuat tanggal pertama dalam bulan yang diinginkan
+  const date = new Date(year, month - 1, 1);
+  
+  // Menggeser tanggal ke awal bulan berikutnya
+  date.setMonth(date.getMonth() + 1);
+  
+  // Mengurangkan 1 hari dari tanggal tersebut untuk mendapatkan tanggal terakhir bulan ini
+  date.setDate(date.getDate() - 1);
+  
+  // Mengembalikan jumlah hari dalam bulan ini
+  return date.getDate();
+}
+
+// Contoh penggunaan fungsi
+const month = 9; // Contoh: September
+const year = 2023; // Contoh tahun
+const daysInMonth = getDaysInMonth(month, year);
+console.log(`Jumlah hari dalam bulan ${month} tahun ${year} adalah ${daysInMonth} hari.`); 
