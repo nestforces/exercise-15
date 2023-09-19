115f822d60d09b5b23914f4c367eae8c02c1e731
nestforces <maulanamahfud2113@gmail.com>
2023-09-19 14:01:25 +0700

    Create inde.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import {
+    isWeekend,
+    findGCD,
+    findLCM,
+    mostCommon
+} from './part1.js';
+
+console.log(isWeekend("2023-09-17"));
+
+console.log(findGCD(36, 18));
+
+console.log(findLCM(4, 9));
+
+console.log(mostCommon("purwadhika"));
