fb7ffa65914bbbe4311119d2a0743e672c3a4e1a
nestforces <71494293+nestforces@users.noreply.github.com>
2023-09-19 15:27:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,3 @@
 const part1 = require('./part1');
 const part2 = require('./part2');
 const part3 = require('./part3');
-
-console.log(part1.isWeekend("2023-09-17"));
-
-console.log(part1.findGCD(36, 18));
-
-console.log(part1.findLCM(4, 9));
-
-console.log(part1.mostCommon("purwadhika"));
